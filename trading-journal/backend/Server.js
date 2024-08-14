f2306a0a351555faf9203be057bfe82a83a6a190
Kirit <kiritrajut@gmail.com>
2024-08-15 02:17:14 +0530

    m

--- trading-journal/backend/Server.js
+++ trading-journal/backend/Server.js
@@ -17,6 +17,12 @@ app.use(cors({
   credentials: true,
 }));
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://xkirit.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 // Routes
 app.use('/api/auth', authRoutes); // Auth routes
