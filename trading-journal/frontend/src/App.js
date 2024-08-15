afe287975c3e35346b3a7ffc0aa19acd2c78bc4f
Kirit <kiritrajut@gmail.com>
2024-08-15 19:02:43 +0530

    browserRouter

--- trading-journal/frontend/src/App.js
+++ trading-journal/frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainLayout from './MainLayout';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
