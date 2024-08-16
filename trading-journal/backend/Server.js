1f98775acbbe6ee68fea679bfc6ddf618de1bf94
Kirit <kiritrajut@gmail.com>
2024-08-16 19:43:15 +0530

    v

--- trading-journal/backend/Server.js
+++ trading-journal/backend/Server.js
@@ -12,7 +12,7 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://xkirit.github.io/TradeScribe/',
   credentials: true,
 }));
 
